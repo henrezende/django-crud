f81842d0ae0b2c56ff4f3c198324900663756c55
henrezende <henrique.64@hotmail.com>
2022-05-07 14:16:00 -0300

    feat: basic config files

---
+++ crudalunos/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for crudalunos project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudalunos.settings')
+
+application = get_wsgi_application()
